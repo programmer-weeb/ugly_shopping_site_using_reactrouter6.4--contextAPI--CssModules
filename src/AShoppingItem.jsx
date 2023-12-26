46373be8b49e623fd6198648a550016db0ed02b7
programmer-weeb <forprogramming8@gmail.com>
2023-12-26 07:29:35 +0200

    Add a Runtime type checking for React props

--- src/AShoppingItem.jsx
+++ src/AShoppingItem.jsx
@@ -1,4 +1,17 @@
+import PropTypes from 'prop-types'
 export default function AShoppingItem() {
 	return <div>AShoppingItem</div>
 }
 
+AShoppingItem.propTypes = {
+	id: PropTypes.number.isRequired,
+	tittle: PropTypes.string.isRequired,
+	price: PropTypes.number.isRequired,
+	description: PropTypes.string.isRequired,
+	category: PropTypes.string,
+	image: PropTypes.string,
+	rating: PropTypes.shape({
+		rate: PropTypes.string,
+		count: PropTypes.number,
+	}),
+}
