841a15abce98d3b8db2228229bb74a15af2d54b5
programmer-weeb <forprogramming8@gmail.com>
2023-12-26 12:07:20 +0200

    AShoppingItem.jsx

--- src/AShoppingItem.jsx
+++ src/AShoppingItem.jsx
@@ -1,6 +1,41 @@
 import PropTypes from 'prop-types'
-export default function AShoppingItem() {
-	return <div>AShoppingItem</div>
+export default function AShoppingItem({
+    id,
+	tittle,
+	price,
+	description,
+	category,
+	image,
+	rating,
+}) {
+	return (
+        <div style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            border: 'none',
+            width: '300px',
+            height: '300px',
+            margin: '10px',
+            padding: '10px',
+
+        }}>
+			{/* make a card for each shopping item */}
+			<h3>{tittle} id: {id} </h3>
+			<p>{price}</p>
+			<p>{description}</p>
+			<p>{category}</p> <br />
+            <img src={image} alt={tittle} style={{ 
+                height: '100px',
+                width: '100px',
+            }} /> <br /> <br />
+			<div className="rating">
+				<p>rating rate: {rating.rate}</p>
+				<p>count of rating: {rating.count}</p>
+			</div>
+		</div>
+	)
 }
 
 AShoppingItem.propTypes = {
