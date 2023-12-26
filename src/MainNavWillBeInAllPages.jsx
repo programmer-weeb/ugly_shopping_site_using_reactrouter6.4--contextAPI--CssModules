0a6f93b2c0100c8046c2cea9b54645e3358a0e90
programmer-weeb <forprogramming8@gmail.com>
2023-12-26 17:34:27 +0200

    Style MainNav using css modules

--- src/MainNavWillBeInAllPages.jsx
+++ src/MainNavWillBeInAllPages.jsx
@@ -1,16 +1,23 @@
 import { Link } from 'react-router-dom'
 import { Outlet } from 'react-router-dom'
+import styles from './modules/MainNav.module.css'
 
 function MainNavWillBeInAllPages() {
-
 	return (
-		<>
-            <h3>here are some links</h3>
-            <Link to='shopping'>Shopping</Link> <br />
-            <Link to='home'>Home</Link> <br />
-            <Link to='/'>Home /</Link>
-            
-		</>
+		<div className={styles.container}>
+			<h3 className={styles.title}>Here are some links =========>>>>>>>>>>>></h3>
+			<Link to="/shopping" className={styles.link}>
+				Shopping
+			</Link>
+			<br />
+			<Link to="/home" className={styles.link}>
+				Home
+			</Link>
+			<br />
+			<Link to="/" className={styles.link}>
+				Home /
+			</Link>
+		</div>
 	)
 }
 
