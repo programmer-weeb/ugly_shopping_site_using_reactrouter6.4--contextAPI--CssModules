45f36dae9f1501e95f720be26844f92eaf5a872f
programmer-weeb <forprogramming8@gmail.com>
2023-12-28 12:51:10 +0200

    Make default quantity == 0

--- src/AShoppingItem.jsx
+++ src/AShoppingItem.jsx
@@ -11,7 +11,7 @@ export default function AShoppingItem({
 	description,
 	image,
 }) {
-    const [quantity, setQuantity] = useState(1)
+    const [quantity, setQuantity] = useState(0)
     const contextData = useContext(AddedToCartItemsContext)
     
 
