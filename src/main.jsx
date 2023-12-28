5b97f0b68d8beeabc2f62fee0694146745b6ef46
programmer-weeb <forprogramming8@gmail.com>
2023-12-28 12:48:27 +0200

    Simplify Main file

--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import { createBrowserRouter } from 'react-router-dom'
-import Home from './Home'
-import Shopping, { shoppingLoader } from './Shopping'
-import ProductPage, { productPageLoader } from './ProductPage'
-import RootLayout from './RootLayout.jsx'
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<RouterProvider
-			router={createBrowserRouter([
-				{
-					path: '/',
-					element: <RootLayout />,
-					children: [
-						{ index: true, element: <Home /> },
-						{ path: 'home', element: <Home /> },
-						{
-							path: 'shopping',
-							element: <Shopping />,
-							loader: shoppingLoader,
-						},
-						{
-							path: 'shopping/:productId',
-							element: <ProductPage />,
-							loader: productPageLoader,
-						},
-					],
-				},
-			])}
-		/>
+		<App />
 	</React.StrictMode>
 )
