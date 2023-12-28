802d36405deda4596207647020a230a3c6324005
programmer-weeb <forprogramming8@gmail.com>
2023-12-28 12:54:14 +0200

    Remove unused line

--- src/Shopping.jsx
+++ src/Shopping.jsx
@@ -4,7 +4,6 @@ import { useNavigate } from 'react-router-dom'
 
 export default function Shopping() {
     const loaderDataFromFakeAPI = useLoaderData()
-    const navigate = useNavigate()
 
 	return (
 		<div>
